dba4b2b4ee39019a562d1a0afb1afa393bf2f9b0
Michael Fabian 'Xaymar' Dirks <info@xaymar.com>
2018-08-04 02:18:39 +0200

    test 3

--- ci/appveyor-test-runner.ps1
+++ ci/appveyor-test-runner.ps1
@@ -8,15 +8,133 @@ class Test
 	[ValidateNotNullOrEmpty()][string]$Category
 	[ValidateNotNullOrEmpty()][string]$Name
 	[ValidateNotNullOrEmpty()][string]$File
+	# Test Info
+	[string]$Framework
+	[string]$Outcome
+	[long]$Duration
+	[String]$ErrorMessage
+	[String]$ErrorStackTrace
+	[String]$StdOut
+	[String]$StdErr
+	
 	
 	Test([string]$c, [string]$n, [string]$f) {
 		$this.Category = $c
 		$this.Name = $n
 		$this.File = $f
+		$this.Framework = "Powershell"
+		$this.Outcome = "None"
+		$this.StdOut = ""
+		$this.StdErr = ""
+		$this.ErrorMessage = ""
+		$this.ErrorStackTrace = ""
 	}
 	
-	[string]DisplayName() {
+	# AppVeyor Invoker
+	Update() {
+		$body = @{
+			testName = $this.GetDisplayName()
+			testFramework = $this.Framework
+			fileName = $this.File
+			durationMilliseconds = $this.Duration
+			outcome = $this.Outcome
+			ErrorMessage = $this.ErrorMessage
+			ErrorStackTrace = $this.ErrorStackTrace
+			StdOut = $this.StdOut
+			StdErr = $this.StdErr
+		}
+		$body_json = $body | ConvertTo-Json -Compress
+		Invoke-RestMethod -Method Put -Uri ("{0}api/tests" -f $Env:APPVEYOR_API_URL) -ContentType "application/json" -Body $body_json
+	}
+	
+	# Setters, Getters
+	[string]GetDisplayName() {
 		return ("{0}/{1}" -f $this.Category, $this.Name)
+	}
+	
+	SetOutcome([string]$o) {
+		$this.Outcome = $o
+		$this.Update()
+	}
+	
+	AppendStdOut([string]$o) {
+		$this.StdOut = ("{0}{1}" -f $this.StdOut, $o)
+		$this.Update()
+	}
+	
+	AppendStdErr([string]$o) {
+		$this.StdErr = ("{0}{1}" -f $this.StdErr, $o)
+		$this.Update()		
+	}
+	
+	AddRunTime([long]$t) {
+		$this.Duration += $t
+		$this.Update()
+	}
+	
+	SetErrorMessage([string]$msg) {
+		$this.ErrorMessage = $msg
+		$this.Update()
+	}
+	
+	# Runners
+	[bool]Execute([int]$it) {
+		$stat_output = ""
+		$safe_output = ""
+		$stat_error = ""
+		$safe_error = ""
+	
+		$this.AppendStdOut(("-- Iteration {0} running --" -f $it))
+		$this.AppendStdErr(("-- Iteration {0} running --" -f $it))
+		
+		$sw = [Diagnostics.Stopwatch]::StartNew()
+		$proc = Start-Process -File "node.exe" -ArgumentList ("`"{0}`"" -f $this.File) -RedirectStandardOutput stdout.log -RedirectStandardError stderr.log -Wait -PassThru -NoNewWindow
+		$sw.Stop()
+		
+		$this.AddRunTime($sw.Elapse.TotalMilliseconds)
+		
+		# Sanitize stdout/stderr.
+		$stat_output = Get-Content -Path stdout.log -Force -Raw -ReadCount 0
+		$stat_error = Get-Content -Path stderr.log -Force -Raw -ReadCount 0
+		if ($stat_output) {
+			$safe_output = ("{0}" -f $stat_output)
+		}
+		if ($stat_error) {
+			$safe_error = ("{0}" -f $stat_error)
+		}		
+		$this.AppendStdOut($safe_output)
+		$this.AppendStdErr($safe_error)
+		
+		if ($proc.ExitCode -eq 0) {
+			return $true
+		} else {
+			return $false
+		}
+	}
+	
+	[bool]Run([int]$its) {
+		$rcode = $true
+		$failed_count = 0
+		
+		$this.SetOutcome("Running");
+		echo ("Test '{0}' running..." -f $this.GetDisplayName())
+		For ($i = 0; $i -lt $its; $i++) {
+			$result = $this.Execute($i)
+			if ($result -eq $false) {
+				$rcode = $false
+				$failed_count++
+			}
+		}
+		if ($failed_count -eq 0) {
+			$this.SetOutcome("Passed")
+		} elseif ($failed_count -eq $its) {
+			$this.SetOutcome("Failed")
+		} else {
+			$this.SetOutcome("Inconclusive")
+			$this.SetErrorMessage(("{0} out of {1} tests failed." -f $failed_count, $its))
+		}
+		
+		return $rcode
 	}
 }
 [Test[]]$Tests = @()
@@ -50,83 +168,19 @@ ForEach($Category in $Categories) {
 		$Tests += $Test
 		
 		# Register to AppVeyor
-		Add-AppveyorTest -Name $Test.DisplayName() -Framework "Powershell" -FileName $Test.File
+		$Test.Update()
 		
-		echo "    Test '$Name' registered."
+		echo "    Test '$Name' registered."		
 	}
 }
 
 # Run Tests
 $ErrorCode = 0
 ForEach($Test in $Tests) {
-	echo ("Running Test '{0}'..." -f $Test.DisplayName())
-	
-	# Set AppVeyor Status
-	Update-AppveyorTest -Name $Test.DisplayName() -Framework "Powershell" -FileName $Test.File -Outcome Running
-	
-	# Variable Storage
-	$stat_total_count = $TestRuns
-	$stat_error_count = 0
-	$stat_time = 0
-	$stat_output = ""
-	$stat_error = ""
-	$safe_output = ""
-	$safe_error = ""
-	$errmsg = ""
-	$stat_outcome = ""
-	
-	# Run 10 Iterations
-	For ($i = 0; $i -lt $stat_total_count; $i++) {
-		$sw = [Diagnostics.Stopwatch]::StartNew()
-		$proc = Start-Process -File "node.exe" -ArgumentList ("`"{0}`"" -f $Test.File) -RedirectStandardOutput stdout.log -RedirectStandardError stderr.log -Wait -PassThru -NoNewWindow
-		$sw.Stop()
-		
-		$stat_time += $sw.Elapsed.TotalMilliseconds
-		if ($proc.ExitCode -ne 0) {
-			$stat_output = Get-Content -Path stdout.log -Force -Raw -ReadCount 0
-			$stat_error = Get-Content -Path stderr.log -Force -Raw -ReadCount 0
-			$stat_error_count += 1
-		} elseif ($stat_error_count -eq 0) {
-			$stat_output = Get-Content -Path stdout.log -Force -Raw -ReadCount 0
-			$stat_error = Get-Content -Path stderr.log -Force -Raw -ReadCount 0
-		}
-	}
-			
-	# Sanitize stdout/stderr.
-	if ($stat_output) {
-		$safe_output = ("{0}" -f $stat_output)
-	}
-	if ($stat_error) {
-		$safe_error = ("{0}" -f $stat_error)
-	}
-	
-	# Set AppVeyor Info
-	$stat_outcome = "Failed"	
-	if ($stat_error_count -eq 0) {
-		$stat_outcome = "Passed"
-	} elseif ($stat_error_count -eq $stat_total_count) {
-		$stat_outcome = "Failed"
-		$ErrorCode = 1
-	} else {
-		$stat_outcome = "Inconclusive"
-		$errmsg = ("{0} out of {1} tests failed." -f $stat_error_count, $stat_total_count)
+	$result = $Test.Run($TestRuns)
+	if ($result -eq $false) {
 		$ErrorCode = 1
 	}
-	
-	$body = @{
-		testName = $Test.DisplayName()
-		testFramework = "Powershell"
-		fileName = $Test.File
-		durationMilliseconds = $stat_time
-		outcome = $stat_outcome
-		ErrorMessage = $errmsg
-		ErrorStackTrace = ""
-		StdOut = $safe_output
-		StdErr = $safe_error
-	}
-	$body_json = $body | ConvertTo-Json -Compress
-	echo $body_json
-	Invoke-RestMethod -Method Put -Uri ("{0}api/tests" -f $Env:APPVEYOR_API_URL) -ContentType "application/json" -Body $body_json
 }
 
 exit $ErrorCode
